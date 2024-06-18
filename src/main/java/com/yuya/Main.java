af7c6a01e09f47b32240ab8c7d82a87f3a78ad2a
yuya-hagiwara <yuyahagi1990@icloud.com>
2024-06-18 15:57:13 +0900

    プロジェクトをセットアップ

--- src/main/java/com/yuya/Main.java
+++ src/main/java/com/yuya/Main.java
@@ -3,17 +3,15 @@ package com.yuya;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
-
-//TIP コードを<b>実行</b>するには、<shortcut actionId="Run"/> を押すか、
-// ガターで <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
 public class Main {
     public static void main(String[] args) {
         Map<String, String> userMap = new HashMap<>();
         userMap.put("高橋", "18歳。生まれて一度も彼女ができた事がありません。");
-        userMap.put("萩原", "27歳。５年間彼女がいなく、結婚願望あり。");
-        userMap.put("岡田", "33歳。既婚者の子供が２人。だけど離婚寸前");
+        userMap.put("萩原", "27歳。5年間彼女がいなく、結婚願望が非常に強い");
+        userMap.put("岡田", "33歳。既婚者で子供が２人。だけど離婚寸前");
         userMap.put("佐伯", "45歳。バツイチ子持ちのシングルファザー。だけど再婚の意欲があり。");
-        userMap.put("高山", "60歳。奥様と30年経ってもラブラブ。だけど刺激を求めている");
+        userMap.put("高山", "43歳。既往歴は脂質異常症。内服薬にて治療中です。");
+        userMap.put("西川", "60歳。奥様と30年経ってもラブラブ。だけど刺激を求めている");
 
         for (String key : userMap.keySet()) {
             System.out.println(key + "さんは" + userMap.get(key));
@@ -33,4 +31,3 @@ public class Main {
         }
     }
 }
-
